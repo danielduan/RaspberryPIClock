fc311cb620363e8c46ff02774155f93d8bd69ec1
Daniel Duan <danielduan88@yahoo.com>
2013-11-06 00:05:33 -0800

    removed excess code

--- clock/CSS Clock_files/clock.js
+++ clock/CSS Clock_files/clock.js
@@ -1,55 +1,12 @@
 (function($){
 
     $(function(){
-        //var hours = $("#hours"), minutes = $("#minutes"), seconds = $("#seconds");
-        
-        //these are default grabs so that the first time it runs, it doesn't throw an error
-        //after that, we use them to cache the current active arm for each group, so that we
-        //don't have to waste time searching
-        //var cHour = $("html"), cMinute = $("html"), cSecond = $("html");
         
         var secondToggle = false;
 
         var setCurrentTime = function(){
             //establish what the time is
             var currentTime = new Date();
-
-            /*
-            var hour = currentTime.getHours() - 1;
-            if(hour == -1){ hour = 11; }
-            var minute = currentTime.getMinutes() - 1;
-            if(minute == -1){ minute = 59; }
-            var second = currentTime.getSeconds() - 1;
-            if(second == -1){ second = 59; }
-            var ampm = "am";
-            if(hour > 11){
-                ampm = "pm";
-                hour = hour-12;
-            }
-            if(hour == 11){
-                ampm = "pm";
-            }
-            
-            
-            //circle clock
-            //
-            //remove the active class, and add it to the new time
-            cHour.removeClass("active");
-            cHour = hours.children(":eq(" + hour + ")").addClass("active");
-            
-            cMinute.removeClass("active");
-            cMinute = minutes.children(":eq(" + minute + ")").addClass("active");
-            
-            cSecond.removeClass("active");
-            cSecond = seconds.children(":eq(" + second + ")").addClass("active");
-            
-            $("body").removeClass("am").removeClass("pm").addClass(ampm);
-            //
-            //end circle clock
-            */
-
-            //start qlocktwo
-            //
 
             var hour = currentTime.getHours();
             var minute = currentTime.getMinutes();
@@ -75,14 +32,6 @@
                         $(".qdot4").addClass("qactive");;
                         break;
                 }
-            //    secondToggle = false;
-            //} else {
-            //    $(".qdot4").removeClass("qactive");
-            //    $(".qdot3").removeClass("qactive");
-            //    $(".qdot2").removeClass("qactive");
-            //    $(".qdot1").removeClass("qactive");
-            //    secondToggle = true;
-            //}
 
             $(".IT").addClass("qactive");
             $(".IS").addClass("qactive");
@@ -182,7 +131,6 @@
                 $(".ELEVEN").removeClass("qactive");
             }
             //
-            //end qlocktwo clock
         
         };
         //set the interval to run each second
